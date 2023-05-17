53f5684b4629111627d8ea0b1431941b9139415e
Marco Dramisino <marco_dramisino@comcast.com>
2023-05-17 16:23:43 +0200

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Marco Dramisino on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
